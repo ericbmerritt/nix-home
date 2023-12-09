b2547b1aeb36befd40d6846abf7ccb91017e2e05
Eric Merritt <eric@merritt.tech>
2023-12-09 09:54:18 -0800

    convert default shell over to nushell

--- common.nix
+++ common.nix
@@ -23,7 +23,67 @@ in {
     ihp-new
     pkgs.cachix
     pkgs.fossil
+    pkgs.nushell
   ];
+
+  home.file.".config/nushell/env.nu".text = ''
+    mkdir ~/.cache/carapace
+    carapace _carapace nushell | save --force ~/.cache/carapace/init.nu
+  '';
+  programs.nushell = {
+    enable = true;
+    # for editing directly to config.nu
+    extraConfig = ''
+       let carapace_completer = {|spans|
+          carapace $spans.0 nushell $spans | from json
+       }
+       $env.config = {
+         show_banner: false,
+         completions: {
+         case_sensitive: false # case-sensitive completions
+           quick: true    # set to false to prevent auto-selecting completions
+           partial: true    # set to false to prevent partial filling of the prompt
+           algorithm: "fuzzy"    # prefix or fuzzy
+           external: {
+              # set to false to prevent nushell looking into $env.PATH to find more suggestions
+              enable: true
+              # set to lower can improve completion performance at the cost of omitting some options
+              max_results: 100
+              completer: $carapace_completer # check 'carapace_completer'
+           }
+         }
+         hooks: {
+           pre_prompt: [{ ||
+             if (which direnv | is-empty) {
+                return
+             }
+
+             direnv export json | from json | default {} | load-env
+           }]
+         }
+      }
+    '';
+    shellAliases = {
+      vi = "hx";
+      vim = "hx";
+      nano = "hx";
+    };
+  };
+  programs.carapace = {
+    enable = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = true;
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
+  };
+
   programs.zsh = {
     enable = true;
     oh-my-zsh = {
@@ -31,7 +91,7 @@ in {
       theme = "bira";
     };
     sessionVariables = {
-      EDITOR = "${pkgs.neovim}/bin/nvim";
+      EDITOR = "${pkgs.helix}/bin/hx";
       ZSH_DISABLE_COMPFIX = "true";
     };
   };
@@ -77,6 +137,9 @@ in {
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      default_shell = "${pkgs.nushell}/bin/nu";
+    };
   };
 
   xdg.configFile."zellij/layouts/landbaron.kdl" = {
